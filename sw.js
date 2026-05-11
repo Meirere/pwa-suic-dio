99c5839cc987243581da430dd0c9957edc920c9b
Meirere <150489816+Meirere@users.noreply.github.com>
2026-05-11 16:06:44 -0300

    Update sw.js
    limpeza de cach

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('laudo-cache').then(function(cache) {
+    caches.open('laudo-cache-v2').then(function(cache) {
       return cache.addAll([
         './',
         './index.html'
